fcc76588958c74019864406e161ecef31bd55e90
LucasCodeur <l.dadamopro@gmail.com>
2025-10-14 16:38:19 +0200

    fix: ex_04, add .replace

--- ex01/src/zombieHorde.cpp
+++ ex01/src/zombieHorde.cpp
@@ -18,8 +18,9 @@ Zombie* zombieHorde( int N, std::string name )
 
 	if (N <= 0)
 		return (NULL);
-	z2 = Zombie(NULL);
 	horde = new Zombie[N];
+	if (horde == NULL)
+		return (NULL);
 	for (size_t	i = 0; i < (size_t)N; i++)
 		horde[i].setName(name);
 	return (horde);
